2d0632c8a1d26dd848229c75d767b8edbcfbf907
Dane Dunlap <danecooperdunlap@gmail.com>
2023-09-30 12:58:00 +0400

    changing back to a form

--- src/App.js
+++ src/App.js
@@ -4,25 +4,22 @@ import './App.css';
 import './LumisForm.css';
 import LumisForm from './LumisForm';
 import RegistrationForm from './RegistrationForm';
-import LoginForm from './LoginForm';
-import HomePage from './HomePage';
+
+
+
 
 function App() {
     return (
-        <Router>
-            <div className="App">
-                <div className="logo">
-                    ✨ Lumis
-                </div>
-                <Routes>
-                    <Route path="/register" element={<RegistrationForm />} />
-                    <Route path="/login" element={<LoginForm />} />
-                    <Route path="/dashboard" element={<LumisForm />} />
-                    <Route path="/" element={<HomePage />} />
-                </Routes>
+        <div className="App">
+            <div className="logo">
+                ✨ Lumis
             </div>
-        </Router>
+            <RegistrationForm />
+        </div>
     );
 }
 
+
 export default App;
+
+
