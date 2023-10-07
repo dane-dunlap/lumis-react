a1d2550af0b555702abcc8819bce00c424d7641c
Dane Dunlap <danecooperdunlap@gmail.com>
2023-10-07 08:52:16 +0400

    Trying routing for summary

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import './LumisForm.css';
 import AppAlertForm from './AppAlertForm';
 import LumisForm from './LumisForm';
 
-
-
-
 function App() {
     return (
-        <div className="App">
-            <div className="logo">
-                ✨ Lumis
+        <Router>
+            <div className="App">
+                <div className="logo">
+                    ✨ Lumis
+                </div>
+                <nav>
+                    <ul>
+                        <li><Link to="/">LumisForm</Link></li>
+                        <li><Link to="/app-alert">App Alert Form</Link></li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route index element={<LumisForm />} />
+                    <Route path="app-alert" element={<AppAlertForm />} />
+                </Routes>
             </div>
-            <AppAlertForm />
-        </div>
+        </Router>
     );
 }
 
-
 export default App;
-
-
